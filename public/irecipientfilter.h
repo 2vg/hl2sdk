7439e22014f0278d5697ba11b859c9295bee9842
2vg <mail@nyaa.gg>
2025-11-05 17:30:52 +0900

    fix: fix

--- public/irecipientfilter.h
+++ public/irecipientfilter.h
@@ -27,7 +27,8 @@ public:
 	virtual bool	IsInitMessage( void ) const = 0;
 
 	virtual const CPlayerBitVec &GetRecipients( void ) const = 0;
-	virtual CPlayerSlot GetPredictedByPlayerSlot( void ) const = 0;
+	//virtual CPlayerSlot GetPredictedByPlayerSlot( void ) const = 0;
+	virtual int UnkFunc() const = 0;
 };
 
 #endif // IRECIPIENTFILTER_H
